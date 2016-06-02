990162ddfd1985e0e892a9ad7c6771d5d4a51229
smdu <dnmamin@gmail.com>
2016-06-02 11:26:38 +0300

    Added toolbar and search botton

--- app/src/main/java/ru/dmamin/remindme/MainActivity.java
+++ app/src/main/java/ru/dmamin/remindme/MainActivity.java
@@ -2,14 +2,32 @@ package ru.dmamin.remindme;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 /**
  * Created by mdu on 02.06.2016.
  */
 public class MainActivity extends Activity {
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
