d6a37ed9c81c4027a1c7c5a8b98fe61477585410
smdu <dnmamin@gmail.com>
2016-06-02 10:14:09 +0300

    Create MainActivity and layout

---
+++ app/src/main/java/ru/dmamin/remindme/MainActivity.java
@@ -0,0 +1,15 @@
+package ru.dmamin.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by mdu on 02.06.2016.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
